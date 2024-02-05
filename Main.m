9902870f55c40b5353be25f463b9f408c0c3f9f8
Division By Two Cellular Automata <158784132+CAdivisionbytwo@users.noreply.github.com>
2024-02-05 22:43:51 +0330

    Add files via upload

---
+++ Main.m
@@ -0,0 +1,166 @@
+function Main()
+
+clc;
+
+nDigits = [10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000]; % number of digits
+MaxIter = 100; % number of iterations for each element of nDigits
+
+fId = fopen('Results1.csv', 'w');
+fprintf(fId, ' ,Alg1_Vec,Alg1_For,Alg3_For,Alg3_ParFor@2,Alg3_ParFor@3,Alg3_ParFor@4');
+
+for nd = 1:length(nDigits)
+    fprintf('Digits #: %d\n', nDigits(nd));
+    fprintf(fId, '\nDigits #: %d\n', nDigits(nd));
+    for iter = 1:MaxIter
+        fprintf('iter:%d\t', iter);
+        longNumber_Array = randi([0, 9], [1, nDigits(nd)]);
+
+        [~, exeTime1_Vec] = Alg1_Vector(longNumber_Array); %parallel with vector
+        [~, exeTime1_For] = Alg1_For(longNumber_Array);  %serial
+        [~, exeTime3_For] = Alg3_For(longNumber_Array); %serial 
+        [~, exeTime3_ParFor2] = Alg3_ParFor(longNumber_Array, 2); % parallel with par
+        [~, exeTime3_ParFor3] = Alg3_ParFor(longNumber_Array, 3);  % parallel with par
+        [~, exeTime3_ParFor4] = Alg3_ParFor(longNumber_Array, 4);  % parallel with par
+
+        fprintf(fId, '%2d,%f,%f,%f,%f,%f,%f,%f,%f,%f\n', iter, exeTime1_Vec, exeTime1_For, exeTime2_Vec, exeTime2_For, exeTime3_For, exeTime3_ParFor1, exeTime3_ParFor2, exeTime3_ParFor3, exeTime3_ParFor4);
+    end
+    fprintf('\n\n');
+end
+
+fclose(fId);
+
+end
+
+function [result, exeTime] = Alg1_Vector(longNumber_Array)
+
+tic;
+
+d = longNumber_Array / 2;
+f = [floor(d), 0];
+m = [0, mod(d, 1) * 10];
+result = f + m;
+
+exeTime = toc;
+
+end
+
+function [result, exeTime] = Alg1_For(longNumber_Array)
+
+tic;
+
+nDigits = length(longNumber_Array);
+result = zeros(1, nDigits+1);
+
+m = 0;
+for i = 1:nDigits
+    d = longNumber_Array(i) / 2;
+    result(i) = floor(d) + m;
+    m = mod(d, 1) * 10;
+end
+
+result(end) = m;
+
+exeTime = toc;
+
+end
+
+function [result, exeTime] = Alg3_For(longNumber_Array)
+
+tic;
+
+nDigits = length(longNumber_Array);
+result = zeros(1, nDigits+1);
+longNumber_Array = [0, longNumber_Array];
+
+for i = 1:nDigits
+    d1 = longNumber_Array(i);
+    d2 = longNumber_Array(i+1);
+    if (mod(d1, 2) == 0)
+        switch (d2)
+            case {0, 1}
+                result(i) = 0;
+            case {2, 3}
+                result(i) = 1;
+            case {4, 5}
+                result(i) = 2;
+            case {6, 7}
+                result(i) = 3;
+            case {8, 9}
+                result(i) = 4;
+        end
+    else
+        switch (d2)
+            case {0, 1}
+                result(i) = 5;
+            case {2, 3}
+                result(i) = 6;
+            case {4, 5}
+                result(i) = 7;
+            case {6, 7}
+                result(i) = 8;
+            case {8, 9}
+                result(i) = 9;
+        end
+    end
+end
+
+if (mod(longNumber_Array(end), 2) == 1)
+    result(end) = 5;
+end
+
+exeTime = toc;
+
+end
+
+function [result, exeTime] = Alg3_ParFor(longNumber_Array, nWorkers)
+
+pp = parpool(nWorkers);
+
+tic;
+
+nDigits = length(longNumber_Array);
+result = zeros(1, nDigits+1);
+longNumber_Array = [0, longNumber_Array];
+temp = longNumber_Array(2:end);
+
+parfor i = 1:nDigits
+    d1 = longNumber_Array(i);
+    d2 = temp(i);
+    if (mod(d1, 2) == 0)
+        switch (d2)
+            case {0, 1}
+                result(i) = 0;
+            case {2, 3}
+                result(i) = 1;
+            case {4, 5}
+                result(i) = 2;
+            case {6, 7}
+                result(i) = 3;
+            case {8, 9}
+                result(i) = 4;
+        end
+    else
+        switch (d2)
+            case {0, 1}
+                result(i) = 5;
+            case {2, 3}
+                result(i) = 6;
+            case {4, 5}
+                result(i) = 7;
+            case {6, 7}
+                result(i) = 8;
+            case {8, 9}
+                result(i) = 9;
+        end
+    end
+end
+
+if (mod(longNumber_Array(end), 2) == 1)
+    result(end) = 5;
+end
+
+exeTime = toc;
+
+delete(pp);
+
+end
